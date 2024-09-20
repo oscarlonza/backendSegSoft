8845005aa9c5b941d4a42ce6a551f5572e424327
Oscar LonZa <fanfleto>
2024-09-20 00:16:33 -0500

    Implementación y ajustes de proceso de encriptación y desencriptación usando AES-GCM

--- services/cryptography.service.js
+++ services/cryptography.service.js
@@ -7,185 +7,111 @@ import { promisify } from 'util'
 import { finished as streamFinished } from 'stream';
 import { generateKeyFromPassword } from "./utils/utils.js"
 
+
+const iv = crypto.randomBytes(16); // Vector de inicialización (IV) aleatorio
+//const iv = crypto.getRandomValues(new Uint8Array(12));
+
 const encryptFile = async (req) => {
 
-    const { error } = encrypt_regex.validate(req)
-    if (error) return response(false, error.details[0].message)
-
-    const inputKey = req.inputKey;
-
-    const txtFileData = req.fileData;
-    console.log('Archivo enviado: ', txtFileData);
-
-    // Decodificar el archivo de Base64
-    const buffer = Buffer.from(txtFileData, 'base64');
-
-    // Convertir el buffer a una cadena de texto
-    const textContent = buffer.toString('utf8');
-    console.log('Archivo ya transformado a texto: ', textContent);
-
-    const password = generateKeyFromPassword(inputKey);
-    const algorithm = 'aes-256-cbc'; // Algoritmo de cifrado
-    const key = Buffer.from(password, 'hex');
-
-    const iv = crypto.randomBytes(16); // Vector de inicialización (IV) aleatorio
-    console.log('IV:' , iv);
-
     try {
-        // Crear el cifrador
-        const cipher = crypto.createCipheriv(algorithm, key, iv);
-        let encrypted = cipher.update(textContent, 'utf8', 'hex');
-        encrypted += cipher.final('hex');
+        const { error } = encrypt_regex.validate(req)
+        if (error) return response(false, error.details[0].message)
+    
+        const inputKey = req.inputKey;
+    
+        const txtFileData = req.fileData;
+        console.log('Archivo enviado: ', txtFileData);
+    
+        // Convertir el buffer a una cadena de texto
+        const textContent = atob(txtFileData);    
+        console.log('Archivo ya transformado a texto: ', textContent);
 
+        // Decodificar el archivo de Base64
+        const buffer = Buffer.from(txtFileData, 'base64');
+    
+        const encoder = new TextEncoder().encode(inputKey);
+    
+        // Hashear el inputKey utilizando SHA-256
+        const hash = await crypto.subtle.digest('SHA-256', encoder);
+    
+        const key = await crypto.subtle.importKey(
+            'raw',
+            hash,
+            { name: 'AES-GCM' },
+            false,
+            ['encrypt']
+        );
+    
+        const encrypted = await crypto.subtle.encrypt(
+            { name: 'AES-GCM', iv },
+            key,
+            buffer
+        );
+    
+        const encryptedString = Buffer.from(encrypted).toString('base64');
+    
         console.log('Archivo cifrado: ', encrypted);
-
-        const fileName = 'File_encriptado.txt';
-        const contentType = 'text/plain';
-
-        // Convertir el string a un buffer
-        const buffer = Buffer.from(encrypted, 'utf8');
-        //console.log(buffer.length);
-
-        // Configurar los encabezados de respuesta para la descarga del archivo
-        const responseData = {
-            headers: {
-                'Content-Disposition': `attachment; filename="${fileName}"`,
-                'Content-Type': contentType,
-                'Content-Length': buffer.length
-            },
-            buffer: buffer
-        };
-        return response(true, 'Archivo cifrado correctamente.', responseData)
-
-    } catch (err) {
-        console.log(err.message);
-
-        return response(false, 'Error al cifrar el archivo.', err.message)
+        console.log('Archivo cifrado: ', encryptedString);
+    
+        const fileName = req.fileName||'encryptedFile.data';
+    
+        //return { iv, encrypted };
+        return response(true, 'Archivo cifrado correctamente.', { encrypted })
+    } catch (error) {
+        return response(false, 'Error al cifrar el archivo.', error.message)        
     }
 
-
-    //const fileData = encrypt_request.inputKey
-
-
-
-    /*const password = generateKeyFromPassword(inputKey);
-
-    const algorithm = 'aes-256-cbc'; // Algoritmo de cifrado
-    const key = Buffer.from(password, 'hex');
-  
-    const iv = crypto.randomBytes(16); // Vector de inicialización (IV) aleatorio
-
-    // Crear los streams para leer y escribir el archivo
-    const input = fs.createReadStream(inputFile);
-    const output = fs.createWriteStream(outputFile);
-
-    try {
-        // Crear el cifrador
-        const cipher = crypto.createCipheriv(algorithm, key, iv);
-
-        // Canalizar el input a través del cifrador y luego al output
-        input.pipe(cipher).pipe(output);
-
-        // Esperar a que la escritura termine
-        await promisify(streamFinished)(output);
-
-        // Añadir IV al principio del archivo cifrado
-        const ivBuffer = Buffer.concat([iv, await fs.promises.readFile(outputFile)]);
-        await fs.promises.writeFile(outputFile, ivBuffer);
-
-        return response(true, 'Archivo cifrado exitosamente.')
-
-    } catch (err) {
-
-        return response(false, 'Error al cifrar el archivo.', err.message)
-    }*/
 }
 
 const decryptFile = async req => {
 
 
-    const { error } = decrypt_regex.validate(req)
-    if (error) return response(false, error.details[0].message)
-
-    const inputKey = req.inputKey;
-
-    const txtFileData = req.fileData;
-    console.log('Archivo enviado: ', txtFileData);
-
-    // Decodificar el archivo de Base64
-    const buffer = Buffer.from(txtFileData, 'base64');
-
-    // Convertir el buffer a una cadena de texto
-    const textContent = buffer.toString('utf8');
-    console.log('Archivo transformado a texto: ', textContent);
-
-    const password = generateKeyFromPassword(inputKey);
-    const algorithm = 'aes-256-cbc'; // Algoritmo de cifrado
-    const key = Buffer.from(password, 'hex');
-
-    //const iv = crypto.randomBytes(16); // Vector de inicialización (IV) aleatorio
-
     try {
+        const { error } = decrypt_regex.validate(req)
+        if (error) return response(false, error.details[0].message)
+    
+        const inputKey = req.inputKey;
+    
+        const txtFileData = req.fileData;
+        console.log('Archivo enviado: ', txtFileData);
+    
+        // Convertir el buffer a una cadena de texto
+        const textContent = atob(txtFileData);
+        console.log('Archivo ya transformado a texto: ', textContent);
 
-        // Extraer el IV (primeros 16 bytes del archivo cifrado)
-        const iv = textContent.slice(0, 16);
-        console.log('IV: ', iv)
-
-        // Extraer los datos cifrados (resto del archivo)
-        const encryptedData = textContent.slice(16);
-
-        const decipher = crypto.createDecipheriv(algorithm, key, iv);
-        let decrypted = decipher.update(encryptedData, 'hex', 'utf8'); // Ajusta el formato según sea necesario
-        decrypted += decipher.final('utf8');
-
-        return response(true, 'Archivo descifrado exitosamente.')
-
-    } catch (err) {
-
-        return response(false, 'Error al descifrar el archivo.', err.message)
+        // Decodificar el archivo de Base64
+        const buffer = Buffer.from(txtFileData, 'base64');
+        
+        const encoder = new TextEncoder().encode(inputKey);
+    
+        // Hashear el inputKey utilizando SHA-256
+        const hash = await crypto.subtle.digest('SHA-256', encoder);
+    
+        const key = await crypto.subtle.importKey(
+            'raw',
+            hash,
+            { name: 'AES-GCM' },
+            false,
+            ['decrypt']
+        );
+    
+        const decrypted = await crypto.subtle.decrypt(
+            { name: 'AES-GCM', iv },
+            key,
+            buffer
+        );
+    
+        const decryptedString = Buffer.from(decrypted).toString('base64');
+        const decryptContent = atob(decryptedString);
+    
+        console.log('Archivo descifrado: ', decrypted);
+        console.log('Archivo descifrado: ', decryptedString);
+    
+        //return decrypted;
+        return response(true, 'Archivo descifrado correctamente.', {decrypted})
+    } catch (error) {
+        return response(false, 'Error al descifrar el archivo.', error.message)       
     }
-
-    /*const inputKey = decrypt_request.inputKey;
-    const inputFile = path.join(decrypt_request.inputFilePath);
-    const outputFile = path.join(decrypt_request.outputFilePath);
-
-    const password = generateKeyFromPassword(inputKey);
-
-    const algorithm = 'aes-256-cbc'; // Algoritmo de cifrado
-    const key = Buffer.from(password, 'hex');
-
-    try {
-        // Leer el archivo cifrado
-        const input = fs.readFileSync(inputFile);
-
-        // Extraer el IV (primeros 16 bytes del archivo cifrado)
-        const iv = input.slice(0, 16);
-
-        // Extraer los datos cifrados (resto del archivo)
-        const encryptedData = input.slice(16);
-
-        // Crear el descifrador
-        const decipher = crypto.createDecipheriv(algorithm, key, iv);
-
-        // Crear el flujo de escritura para el archivo descifrado
-        const output = fs.createWriteStream(outputFile);
-
-        // Desencriptar el archivo
-        const buffer = Buffer.concat([decipher.update(encryptedData), decipher.final()]);
-
-        // Usar el flujo de escritura para guardar los datos descifrados
-        output.write(buffer);
-        output.end();
-
-        // Esperar a que la escritura termine
-        await promisify(streamFinished)(output);
-
-        return response(true, 'Archivo descifrado exitosamente.')
-
-    } catch (err) {
-
-        return response(false, 'Error al descifrar el archivo.', err.message)
-    }*/
 }
 
 export { encryptFile, decryptFile }
