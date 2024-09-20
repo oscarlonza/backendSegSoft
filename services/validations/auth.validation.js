713bac8f9c4b82e4958547b0dc10774caded7928
hspnet <hpeluffo@uninorte.edu.co>
2024-09-20 09:15:46 -0500

    se implementa mejora para la asignación de contraseña con los caracteres especiales, se tienen en cuenta los siguientes !@#$%^&.*

--- services/validations/auth.validation.js
+++ services/validations/auth.validation.js
@@ -4,7 +4,7 @@ const user_regex = Joi.object({
     name: Joi.string().min(4).max(16).required(),
     nickname: Joi.string().min(4).max(16).required(),
     cel: Joi.string().min(10).max(15).required(),
-    password: Joi.string().min(8).max(256).required().pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,256}$/)
+    password: Joi.string().min(8).max(256).required().pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&.*]).{8,256}$/)
     .messages({
         'string.pattern.base': 'La contraseña debe tener al menos 8 caracteres, incluyendo al menos 1 número, 1 letra minúscula, 1 letra mayúscula y 1 carácter especial.',
     }),
